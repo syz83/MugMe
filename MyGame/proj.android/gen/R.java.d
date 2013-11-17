5db9338d2691e27c13791fbce82370111fd0afe7
Steven Zhang <stevenzh2012@gmail.com>
2013-11-17 00:27:02 -0600

    Working Project

---
+++ MyGame/proj.android/gen/R.java.d
@@ -0,0 +1,9 @@
+D:\Development\cocos2d-x-3.0alpha0\projects\MyGame\proj.android\gen\com\MyCompany\AwesomeGame\R.java \
+ : D:\Development\cocos2d-x-3.0alpha0\projects\MyGame\proj.android\res\drawable-hdpi\icon.png \
+D:\Development\cocos2d-x-3.0alpha0\projects\MyGame\proj.android\res\drawable-ldpi\icon.png \
+D:\Development\cocos2d-x-3.0alpha0\projects\MyGame\proj.android\res\drawable-mdpi\icon.png \
+D:\Development\cocos2d-x-3.0alpha0\projects\MyGame\proj.android\res\values\strings.xml \
+D:\Development\cocos2d-x-3.0alpha0\projects\MyGame\proj.android\bin\res\drawable-hdpi\icon.png \
+D:\Development\cocos2d-x-3.0alpha0\projects\MyGame\proj.android\bin\res\drawable-ldpi\icon.png \
+D:\Development\cocos2d-x-3.0alpha0\projects\MyGame\proj.android\bin\res\drawable-mdpi\icon.png \
+D:\Development\cocos2d-x-3.0alpha0\projects\MyGame\proj.android\bin\AndroidManifest.xml \
